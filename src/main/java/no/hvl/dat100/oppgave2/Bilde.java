3939526079aa6e03153d796500f51ed1023a2c78
Koding667278 <667278@stud.hvl.no>
2025-11-12 17:00:54 +0100

    Update Bilde.java
    Første forsøk

--- src/main/java/no/hvl/dat100/oppgave2/Bilde.java
+++ src/main/java/no/hvl/dat100/oppgave2/Bilde.java
@@ -4,35 +4,40 @@ import no.hvl.dat100.common.TODO;
 
 public class Bilde extends Tekst {
 
-	// TODO - objekt variable
+	private String url;
 	
 	public Bilde(int id, String bruker, String dato, String tekst, String url) {
+		super(id, bruker, dato, tekst);
+		this.url = url;
+		
 		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
 	}
 
 	public Bilde(int id, String bruker, String dato, int likes, String tekst, String url) {
+		super(id, bruker, dato, likes, tekst);
+		this.url = url;
+		
 		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
 	}
 	
 	public String getUrl() {
+		return url;
+		
 		throw new UnsupportedOperationException(TODO.method());
 
 	}
 
 	public void setUrl(String url) {
+		this.url = url;
+		
 		throw new UnsupportedOperationException(TODO.method());
 	}
 
 	@Override
 	public String toString() {
+		return "BILDE\n" + super.getId() + "\n" + super.getBruker() + "\n" + super.getDato() + "\n" + super.getLikes() + "\n" + super.getTekst() + "\n" + url + "\n"; 
+		
 		throw new UnsupportedOperationException(TODO.method ());
 
-	}
-
-	// Metoden nedenfor er kun for valgfri oppgave 6
-	public String toHTML() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-				
 	}
 }
