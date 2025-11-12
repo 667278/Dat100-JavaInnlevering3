08568d8dbccc987332f8c99f4c1f69c742c093b6
Koding667278 <667278@stud.hvl.no>
2025-11-12 17:38:23 +0100

    Update SkrivBlogg.java
    Første Forsøk

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -9,7 +9,18 @@ import no.hvl.dat100.oppgave3.*;
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+		boolean skrevet = true;
+		PrintWriter writer = null;
 
+		try {
+			writer = new PrintWriter(mappe + filnavn);
+			writer.print(samling.toString());
+			writer.close();
+		} catch (FileNotFoundExecption e) {
+			System.out.println("Fant ikkje fil, så skriver ikkje ut");
+			Skrevet = false;
+		}
+		return skrevet;
 		throw new UnsupportedOperationException(TODO.method());
 	}
 }
